240039633bf0951d118743d73bdd7387987b511e
jihoon <lanslot84@gmail.com>
2023-10-09 22:55:21 +0900

    ✨ FEAT : added function of fetch and render datas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import design from './__resources/design/desktop-design.jpg';
 import divider from './__resources/images/pattern-divider-desktop.svg';
 import dice from './__resources/images/icon-dice.svg';
-
-
 import './App.css';
 
+const API_URL = 'https://api.adviceslip.com/advice';
+let res = {};
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={design} />
         <div className="card">
           <label>Advice <em>117</em></label>
           <p className="advice-paragraph">it is easy to sit up and take notice, what's difficult is getting up and taking action.</p>
           <img src={divider} />
-          <button className="dice-button"><img src={dice} /></button>
+          <button 
+            className="dice-button" 
+            onClick={() => getFetchData() }>
+              <img src={dice} />
+          </button>
+          
         </div>
-      </header>
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/elice-study-first/advice-generator-jihoonkim" target="_blank">Jihoon Kim</a>.
+        </div>
     </div>
+    
   );
+}
+
+export function getFetchData() {
+  const diceButton = document.querySelector('button.dice-button');
+  diceButton.classList.add('processing');
+
+  fetch(API_URL).then( response => response.json())
+    .then( data => {
+      res = data.slip
+      
+      //console.log(res);
+      setTimeout( () => {
+        return generateAdvice(res)
+      }, 2000)
+    })
+}
+
+export function generateAdvice(res) {
+  const diceButton = document.querySelector('button.dice-button');
+  diceButton.classList.remove('processing');
+
+  const idElement = document.querySelector('label em');
+  const paragraphElement = document.querySelector('.advice-paragraph');
+
+  idElement.innerHTML = res.id;
+  paragraphElement.innerHTML = res.advice;
 }
 
 export default App;
